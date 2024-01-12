be45f28e7aebcd15ac2373a7fd2d8a3c2be30fbf
Valera6 <v79166789533@gmail.com>
2024-01-12 19:29:07 +0000

    extract price from websocket stream

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use futures_util::StreamExt;
-use tokio::io::AsyncWriteExt;
+use serde_json::Value;
 use tokio_tungstenite::connect_async;
 
 #[tokio::main]
@@ -7,12 +7,22 @@ async fn main() {
 	let address = "wss://fstream.binance.com/ws/btcusdt@markPrice";
 	let url = url::Url::parse(address).unwrap();
 	let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-	println!("Connected");
+	println!(" ++ Connected ++ ");
 	let (_, read) = ws_stream.split();
 	let ws_to_stdout = {
 		read.for_each(|message| async {
 			let data = message.unwrap().into_data();
-			tokio::io::stdout().write_all(&data).await.unwrap();
+			match serde_json::from_slice::<Value>(&data) {
+				Ok(json) => {
+					if let Some(price_str) = json.get("p") {
+						let price: f32 = price_str.as_str().unwrap().parse().unwrap();
+						println!("{}", price);
+					}
+				}
+				Err(e) => {
+					println!("Failed to parse message as JSON: {}", e);
+				}
+			}
 		})
 	};
 	ws_to_stdout.await;

171c76c84f6d8644291504c24ca25c043edc9332
valeratrades <v79166789533@gmail.com>
2024-10-26 15:48:34 +0200

    fixup!

--- default.nix
+++ default.nix
@@ -1,14 +1,19 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 let
-	manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
 pkgs.rustPlatform.buildRustPackage rec {
   pname = manifest.name;
   version = manifest.version;
-	
-	#buildInputs = with pkgs; [openssl openssl.dev]; # rustPlatform.bindgenHook];
-	#nativeBuildInputs = with pkgs; [pkg-config];
-	#env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+  buildInputs = with pkgs; [
+    openssl
+    openssl.dev
+  ]; # rustPlatform.bindgenHook];
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;

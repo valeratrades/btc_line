cfa3f914665d6b7a1eab7096993cf637f7977d2f
Valera <valeratrades@gmail.com>
2025-10-14 16:50:30 +0000

    wip: tmp: persist current additional_line in example for experimentation

--- examples/test.rs
+++ examples/test.rs
@@ -1,19 +1,133 @@
-use tokio::{
-	select,
-	time::{Duration, interval},
-};
+use std::sync::{Arc, Mutex};
+
+use btc_line::config::AppConfig;
+use color_eyre::eyre::{Result, bail};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use tracing::{debug, info};
+use v_utils::NowThen;
+
+//TODO!: implement tiny graphics
+#[derive(Default, Debug, Serialize)]
+pub struct AdditionalLine {
+	open_interest_change: Option<NowThen>,
+	btc_volume_change: Option<NowThen>,
+}
+
+impl AdditionalLine {
+	pub fn display(&self, config: &AppConfig) -> String {
+		let mut oi_str = self.open_interest_change.as_ref().map_or("None".to_string(), |v| v.to_string());
+		let mut v_str = self.btc_volume_change.as_ref().map_or("None".to_string(), |v| v.to_string());
+
+		if config.label {
+			oi_str = format!("OI:{oi_str}");
+			v_str = format!("V:{v_str}");
+		}
+		format!("{oi_str} {v_str}")
+	}
+
+	pub async fn collect(self_arc: Arc<Mutex<Self>>, config: &AppConfig) {
+		let comparison_offset_h = config.comparison_offset_h;
+
+		let client = reqwest::Client::new();
+		let open_interest_change_handler = get_open_interest_change(&client, "BTCUSDT", comparison_offset_h);
+		let btc_volume_change_handler = get_btc_volume_change(&client, comparison_offset_h);
+
+		let mut new_state = AdditionalLine::default(); // slight perf hit in favor of debuggability
+		let (oi_result, volume_result) = tokio::join!(open_interest_change_handler, btc_volume_change_handler);
+
+		match oi_result {
+			Ok(open_interest_change) => new_state.open_interest_change = Some(open_interest_change),
+			Err(e) => {
+				debug!("Failed to get Open Interest: {e}");
+			}
+		};
+		match volume_result {
+			Ok(btc_volume_change) => new_state.btc_volume_change = Some(btc_volume_change),
+			Err(e) => {
+				debug!("Failed to get BTC Volume: {e}");
+			}
+		};
+
+		info!(?new_state);
+		*self_arc.lock().unwrap() = new_state;
+	}
+}
+
+/// Compares btc OI today against 24h ago (changes based on `comparison_offset_h`)
+async fn get_open_interest_change(client: &reqwest::Client, symbol: &str, comparison_offset_h: usize) -> Result<NowThen> {
+	let url = format!(
+		"https://fapi.binance.com/futures/data/openInterestHist?symbol={}&period=5m&limit={}",
+		symbol,
+		comparison_offset_h * 12 + 1
+	);
+
+	let response = client.get(&url).send().await?;
+	if response.status().is_success() {
+		let json_string = response.text().await?;
+		let r: Vec<OpenInterestHist> = serde_json::from_str(&json_string)?;
+
+		let now: f64 = r[0].sumOpenInterestValue.parse()?;
+		let then: f64 = r[r.len() - 1].sumOpenInterestValue.parse()?;
+
+		Ok(NowThen::new(now, then))
+	} else {
+		bail!("Failed to get Open Interest Change: {}", response.status())
+	}
+}
+
+/// Compares two last periods of `comparison_offset_h` hours. Default is yesterday against the day before.
+async fn get_btc_volume_change(client: &reqwest::Client, comparison_offset_h: usize) -> Result<NowThen> {
+	let interval = comparison_offset_h * 12;
+	let url = format!("https://fapi.binance.com/fapi/v1/klines?symbol=BTCUSDT&interval=5m&limit={}", interval + interval);
+
+	let response = client.get(&url).send().await?;
+	if response.status().is_success() {
+		let json_string = response.text().await?;
+		let r: Vec<Kline> = serde_json::from_str(&json_string)?;
+
+		let split = r.split_at(interval);
+		let now: f64 = split.0.iter().map(|v| v.quote_asset_volume.parse::<f64>().unwrap()).sum();
+		let then: f64 = split.1.iter().map(|v| v.quote_asset_volume.parse::<f64>().unwrap()).sum();
+
+		Ok(NowThen::new(now, then))
+	} else {
+		bail!("Failed to get BTC Volume: {}", response.status())
+	}
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(non_snake_case, dead_code)]
+struct OpenInterestHist {
+	symbol: String,
+	sumOpenInterest: String,
+	sumOpenInterestValue: String,
+	timestamp: i64,
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(non_snake_case, dead_code)]
+struct Kline {
+	open_time: i64,
+	open: String,
+	high: String,
+	low: String,
+	close: String,
+	volume: String,
+	close_time: i64,
+	quote_asset_volume: String,
+	number_of_trades: usize,
+	taker_buy_base_asset_volume: String,
+	taker_buy_quote_asset_volume: String,
+	ignore: String,
+}
 
 #[tokio::main]
 async fn main() {
-	// trying to figure out correct select loop for main logic: some requests take very long, don't want them to be aborted
+	let client = Client::new();
 
-	let mut hello_tick = interval(Duration::from_secs(1));
-	let mut world_tick = interval(Duration::from_secs(5));
+	let interest_change = get_open_interest_change(&client, "BTCUSDT", 24).await;
+	let volume_change = get_btc_volume_change(&client, 24).await;
 
-	loop {
-		select! {
-			_ = hello_tick.tick() => println!("hello"),
-			_ = world_tick.tick() => println!("world"),
-		}
-	}
+	dbg!(&interest_change, &volume_change);
 }
